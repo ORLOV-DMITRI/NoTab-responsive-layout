df975b6a311d59b73caa5812cf87e8c9ae612ee9
Dmitriy Orlov <dmitri.orlov.anatolevich@gmail.com>
2023-04-16 14:47:43 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// const cookie = document.querySelector('.cookie');
+
+// setTimeout(() => {
+//   cookie.classList.add('cookie-appearance');
+// }, 1000);
+// Приве
+
+const inputName = document.querySelector(".get-touch__name");
+const inputVenueName = document.querySelector(".get-touch__venue-name");
+const inputVenueCity = document.querySelector(".get-touch__venue-city");
+const inputStateCity = document.querySelector(".get-touch__state-city");
+const inputEmail = document.querySelector(".get-touch__email");
+const inputSubject = document.querySelector(".get-touch__subject");
+const inputMessage = document.querySelector(".get-touch__message");
+
+const input = document.querySelector(".get-touch__input");
+const sendBtn = document.querySelector(".get-touch__btn");
+console.log(input.children[0].value);
+
+// sendBtn.addEventListener("click", () => {
+//   for (let i = 0; i < input.children.length; i++) {
+//     if (input.children[i].value === "") {
+//       console.log(input.children[2].classList.add("error"));
+//     }
+//   }
+//   if(inputVenueCity.value )
+// });
