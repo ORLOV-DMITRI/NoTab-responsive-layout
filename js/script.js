3766eba8873e2ee8bf5ac081f6bcbdd3718c2e03
Dmitri Orlov <dmitri.orlov.anatolevich@gmail.com>
2023-04-23 08:43:17 +0300

    vd

--- js/script.js
+++ js/script.js
@@ -1,38 +1,34 @@
 const cookie = document.querySelector(".cookie");
 const btnCookie = document.querySelector(".cookie__btn");
 
-setTimeout(() => {
-  cookie.classList.add("show-cookie");
-}, 1500);
+// setTimeout(() => {
+//   cookie.classList.add("show-cookie");
+// }, 1500);
 btnCookie.addEventListener("click", () => {
   cookie.classList.remove("show-cookie");
 });
 
 const phone = document.querySelector(".mission__img");
 const text = document.querySelector(".mission__content");
+let start = 70;
+let ratio = 10;
 
 const options = {
-  rootMargin: "0px",
-  threshold: [0.5],
+  rootMargin: "0px 0px 100px 0px  ",
+  threshold: [1],
 };
 const trueCallback = function (entries, observer) {
-  entries.forEach((entry) => {
-    const { target, isIntersecting } = entry;
+  entries.forEach(({ isIntersecting, intersectionRatio }) => {
     if (window.innerWidth >= 940) {
       if (isIntersecting) {
-        phone.classList.add("run");
-        text.classList.add("text-trans");
-        setTimeout(() => {
-          phone.classList.add("rotate");
-        }, 1000);
-      } else {
-        phone.classList.remove("rotate");
-        text.classList.remove("text-trans");
-        phone.classList.remove("run");
+        if (intersectionRatio >= 0.45) {
+          phone.classList.add("run");
+          text.classList.add("text-trans");
+        }
       }
     }
   });
 };
 const observer = new IntersectionObserver(trueCallback, options);
-const target = document.querySelector(".mission");
+const target = document.querySelector(".mission__inner");
 observer.observe(target);
